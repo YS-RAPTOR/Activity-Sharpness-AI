44f94dfe03faccec3062a07e0f8defa808621c98
YS-RAPTOR <yashan.sumanaratne@gmail.com>
2025-05-18 20:37:18 +1000

    Small changes to eda.py

--- models/eda.py
+++ models/eda.py
@@ -12,7 +12,7 @@ pd.set_option("display.max_rows", None)
 pd.set_option("display.width", 1000000000)
 
 drop_list = [
-    # Number of occurances are the same
+    # Number of occurrences are the same
     "SV Pelvis x",
     "SV Pelvis y",
     "SV Pelvis z",
@@ -79,8 +79,6 @@ for feature in drop_list:
     df.drop(columns=feature, inplace=True)
     features.remove(feature)
 
-print(features)
-
 # Check if there are any features that can be converted to a categorical type
 # Check if there are features that are constants
 count_threshold = 100
@@ -94,92 +92,85 @@ if ANALYSIS:
             print("")
 
 # Class Distribution
-class_distribution = df[target].value_counts()
-print("Class Distribution: ")
-print(class_distribution, "\n\n")
 
 if ANALYSIS:
+    class_distribution = df[target].value_counts()
     class_distribution.plot(kind="bar")
+    class_weights = class_distribution.sum() / class_distribution
+    print("Class Weights: \n\n", class_weights)
 
-# Adjust class weights
-class_weights = class_distribution.sum() / class_distribution
-print(class_weights)
-
-# Shuffled dataframe slice
 df = df[features]
-shuffled_df = df.sample(n=10000).reset_index(drop=True)
-
-print(len(shuffled_df))
-
 # EDA
-already_done: Dict[Tuple[str, str], bool] = {}
-count = 0
-corr = df.corr()
-no_of_subplots = 4
+if EDA:
+    shuffled_df = df.sample(n=10000).reset_index(drop=True)
+    already_done: Dict[Tuple[str, str], bool] = {}
+    count = 0
+    corr = df.corr()
+    no_of_subplots = 4
 
-axs = None
-fig = None
+    axs = None
+    fig = None
 
-for f1 in features:
-    for f2 in features:
-        if f1 == f2:
-            continue
-
-        if (f1, f2) in already_done or (f2, f1) in already_done:
-            continue
-
-        already_done[(f1, f2)] = True
-
-        try:
-            x = shuffled_df[f1]
-            y = shuffled_df[f2]
-
-            correlation = corr[f1][f2]
-
-            if abs(correlation) < 0.80 or math.isnan(correlation):
+    for f1 in features:
+        for f2 in features:
+            if f1 == f2:
                 continue
 
-            if axs is None or fig is None:
-                fig, axs = plt.subplots(
-                    no_of_subplots, no_of_subplots, figsize=(20, 10)
-                )
-                axs = axs.flatten()
+            if (f1, f2) in already_done or (f2, f1) in already_done:
+                continue
 
-            slope, intercept, r_value, p_value, std_err = linregress(x, y)
-            line = slope * x + intercept
+            already_done[(f1, f2)] = True
 
-            ax = axs[count % (no_of_subplots * no_of_subplots)]
-            ax.scatter(x, y, label="Data Points")
-            ax.plot(x, line, color="red", label=f"Fit Line (r={r_value:.2f})")
+            try:
+                x = shuffled_df[f1]
+                y = shuffled_df[f2]
 
-            ax.set_xlabel(f1)
-            ax.set_ylabel(f2)
-            ax.set_title(f"{f1} vs {f2}: {correlation:.2f}")
+                correlation = corr[f1][f2]
 
-            count += 1
+                if abs(correlation) < 0.80 or math.isnan(correlation):
+                    continue
 
-            if (count) % (no_of_subplots * no_of_subplots) == 0:
-                plt.tight_layout()
-                plt.show()
-                fig = None
-                axs = None
+                if axs is None or fig is None:
+                    fig, axs = plt.subplots(
+                        no_of_subplots, no_of_subplots, figsize=(20, 10)
+                    )
+                    axs = axs.flatten()
 
-        except Exception:
-            continue
+                slope, intercept, r_value, p_value, std_err = linregress(x, y)
+                line = slope * x + intercept
 
-if fig is not None:
-    plt.tight_layout()
-    plt.show()
+                ax = axs[count % (no_of_subplots * no_of_subplots)]
+                ax.scatter(x, y, label="Data Points")
+                ax.plot(x, line, color="red", label=f"Fit Line (r={r_value:.2f})")
 
+                ax.set_xlabel(f1)
+                ax.set_ylabel(f2)
+                ax.set_title(f"{f1} vs {f2}: {correlation:.2f}")
 
-print("Is Null: ")
-print(df.isnull().sum(), "\n\n")
+                count += 1
 
-print("Is NaN: ")
-print(df.isna().sum(), "\n\n")
+                if (count) % (no_of_subplots * no_of_subplots) == 0:
+                    plt.tight_layout()
+                    plt.show()
+                    fig = None
+                    axs = None
 
-print("Is Inf: ")
-print(np.isinf(df).sum(), "\n\n")
+            except Exception:
+                continue
 
-print("Duplicates: ")
-print(df.duplicated().sum(), "\n\n")
+    if fig is not None:
+        plt.tight_layout()
+        plt.show()
+
+if ANALYSIS:
+    print("Is Null: ")
+    print(df.isnull().sum(), "\n\n")
+
+    print("Is NaN: ")
+    print(df.isna().sum(), "\n\n")
+
+    print("Is Inf: ")
+    print(np.isinf(df).sum(), "\n\n")
+
+    print("Duplicates: ")
+    print(df.duplicated().sum(), "\n\n")
