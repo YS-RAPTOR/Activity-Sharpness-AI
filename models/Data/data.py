2154559bfdb904d21e48f45bd672439f31e323d5
Yashan <yashan.sumanaratne@gmail.com>
2025-05-30 13:31:44 +1000

    Create data.py in Data folder

---
+++ models/Data/data.py
@@ -0,0 +1,3 @@
+PREPROCESS_DATA_DIR = "C:\\Users\\102838834\\OneDrive - Swinburne University\\Year 4\\Artificial Intelligence for Engineering\\Project\\Data\\Raw"
+PROCESSED_DATA_DIR = "C:\\Users\\102838834\\OneDrive - Swinburne University\\Year 4\\Artificial Intelligence for Engineering\\Project\\Data\\Processed"
+DATA = "C:\\Users\\102838834\\OneDrive - Swinburne University\\Year 4\\Artificial Intelligence for Engineering\\Project\\Data\\Processed\\data.csv"
